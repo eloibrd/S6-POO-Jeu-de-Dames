057cf7b92eb9f9b284a29094eb037bbea9fb9cc7
Eloi Bernard <eloi.bernard@tpi24.cpe.fr>
2021-02-08 14:11:55 +0100

    fin atelier 1 - ModelImplementor

--- model/ModelImplementor.java
+++ model/ModelImplementor.java
@@ -10,18 +10,18 @@ import nutsAndBolts.PieceSquareColor;
  * @author francoise.perrin
  * 
  * Cete classe fabrique et stocke toutes les PieceModel du Model dans une collection 
- * elle est donc responsable de rechercher et mettre à jour les PieceModel (leur position)
- * En réalité, elle délègue à une fabrique le soin de fabriquer les bonnes PieceModel 
- * avec les bonnes coordonnées
+ * elle est donc responsable de rechercher et mettre ï¿½ jour les PieceModel (leur position)
+ * En rï¿½alitï¿½, elle dï¿½lï¿½gue ï¿½ une fabrique le soin de fabriquer les bonnes PieceModel 
+ * avec les bonnes coordonnï¿½es
  * 
- * En revanche, elle n'est pas responsable des algorithme métiers liés au déplacement des pièces
- * (responsabilité de la classe Model)
+ * En revanche, elle n'est pas responsable des algorithme mï¿½tiers liï¿½s au dï¿½placement des piï¿½ces
+ * (responsabilitï¿½ de la classe Model)
  */
 public class ModelImplementor {
 
-	// la collection de pièces en jeu - mélange noires et blanches
+	// la collection de piï¿½ces en jeu - mï¿½lange noires et blanches
 	private Collection<PieceModel> pieces ;	
-
+	
 	public ModelImplementor() {
 		super();
 
@@ -31,7 +31,10 @@ public class ModelImplementor {
 	public PieceSquareColor getPieceColor(Coord coord) {
 		PieceSquareColor color = null;
 
-		// TODO Atelier 1
+		PieceModel piece = findPiece(coord);
+		if(piece!=null) {
+			color=piece.getPieceColor();
+		}
 		
 		return color;
 	}
@@ -39,7 +42,9 @@ public class ModelImplementor {
 	public boolean isPiecehere(Coord coord) {
 		boolean isPiecehere = false;
 
-		// TODO Atelier 1
+		if(findPiece(coord)!=null) {
+			isPiecehere=true;
+		}
 		
 		return isPiecehere;
 	}
@@ -48,7 +53,16 @@ public class ModelImplementor {
 
 		boolean isMovePieceOk = false;
 
-		// TODO Atelier 1
+		if(Coord.coordonnees_valides(targetCoord)) {
+			if(!isPiecehere(targetCoord)) {
+				PieceModel piece = findPiece(targetCoord);
+				if(piece!=null) {
+					if(piece.isMoveOk(targetCoord, isPieceToTake)) {
+						isMovePieceOk=true;
+					}
+				}
+			}
+		}
 		
 		return isMovePieceOk;
 	}
@@ -57,15 +71,16 @@ public class ModelImplementor {
 	public boolean movePiece(Coord initCoord, Coord targetCoord) {
 
 		boolean isMovePieceDone = false;
-
-		// TODO Atelier 1
-		
+		boolean isPieceToTake = (initCoord.getColonne() == targetCoord.getColonne()+2 || initCoord.getColonne() == targetCoord.getColonne()-2) && (initCoord.getLigne() == targetCoord.getLigne()-2 || initCoord.getLigne() == targetCoord.getLigne()+2);
+		if(isMovePieceOk(initCoord, targetCoord, isPieceToTake)) {
+			findPiece(initCoord).move(targetCoord);
+			isMovePieceDone = false;
+		}
 		return isMovePieceDone;
 	}
 
-	public void removePiece(Coord pieceToTakeCoord) {
-
-		// TODO Atelier 2
+	public void removePiece(Coord pieceToTakeCoord) {		
+		this.pieces.remove(findPiece(pieceToTakeCoord));		
 		
 	}
 
@@ -81,15 +96,17 @@ public class ModelImplementor {
 	
 	/**
 	 * @param coord
-	 * @return la pièce qui se trouve aux coordonnées indiquées
+	 * @return la piï¿½ce qui se trouve aux coordonnï¿½es indiquï¿½es
 	 */
-	 PieceModel findPiece(Coord coord) {		// TODO : mettre en "private" après test unitaires
+	 private PieceModel findPiece(Coord coord) {
 		 
-		PieceModel findPiece = null;
-
-		// TODO Atelier 1
-		
-		return findPiece;
+		 PieceModel pieceResult =null;
+			for(PieceModel piece : this.pieces) {
+				if(piece.hasThisCoord(coord)) {
+					pieceResult = piece;
+				}
+			}
+			return pieceResult;
 	}
 
 
@@ -97,8 +114,8 @@ public class ModelImplementor {
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 * 
-	 * La méthode toStrong() retourne une représentation 
-	 * de la liste de pièces sous forme d'un tableau 2D
+	 * La mï¿½thode toStrong() retourne une reprï¿½sentation 
+	 * de la liste de piï¿½ces sous forme d'un tableau 2D
 	 * 
 	 */
 	public String toString() {
@@ -107,7 +124,7 @@ public class ModelImplementor {
 		String st = "";
 		String[][] damier = new String[ModelConfig.LENGTH][ModelConfig.LENGTH];
 
-//		// création d'un tableau 2D avec les noms des pièces à partir de la liste de pièces
+//		// crï¿½ation d'un tableau 2D avec les noms des piï¿½ces ï¿½ partir de la liste de piï¿½ces
 //		for(PieceModel piece : this.pieces) {
 //
 //			PieceSquareColor color = piece.getPieceColor();
@@ -118,7 +135,7 @@ public class ModelImplementor {
 //			damier[lig][col ] = stColor ;
 //		}
 
-		// Affichage du tableau formatté
+		// Affichage du tableau formattï¿½
 		st = "     a      b      c      d      e      f      g      h      i      j\n";
 		for ( int lig = 9; lig >=0 ; lig--) {
 			st += (lig+1) + "  ";
