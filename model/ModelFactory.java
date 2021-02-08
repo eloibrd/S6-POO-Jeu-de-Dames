8d7d802a85f2a6bbb62483ed8bdc05ff5e48c317
Eloi Bernard <eloi.bernard@tpg02.cpe.fr>
2021-02-08 09:14:35 +0100

    init repo

---
+++ model/ModelFactory.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import nutsAndBolts.PieceSquareColor;
+
+public class ModelFactory {
+
+	public static Collection<PieceModel> createPieceModelCollection() {
+		
+		Collection<PieceModel> pieces = new LinkedList<PieceModel>();
+
+		// Création des pion blancs et ajout dans la collection de pièces
+		for ( Coord coord : ModelConfig.WHITE_PIECE_COORDS){
+			pieces.add(new PawnModel(coord, PieceSquareColor.WHITE));
+		}
+
+		// Création des pions noirs et ajout dans la collection de pièces
+		for ( Coord coord : ModelConfig.BLACK_PIECE_COORDS){
+			pieces.add(new PawnModel(coord, PieceSquareColor.BLACK));
+		}
+		return pieces;
+	}
+}
